60447ca9d1944ddc73b19f727e9a0f0336735c59
JasonChen523 <168566316+JasonChen523@users.noreply.github.com>
2026-04-17 11:46:44 +0800

    Delete ExplorerMode.py

--- ExplorerMode.py
+++
@@ -1,2 +0,0 @@
-#Explorer Mode is a challenging mode where only adjacent grids are visible to the player, just like exploring a real-life 3d maze.
-#The maze solver for this mode is to be added. Advanced functions including path memory and exit deduction.
